3f77963af7df32132bc05208ef6a015e22329fed
Henry Yang <mail@xiaoming.se>
2018-07-22 18:18:32 +0800

    Add Initial Files based on SpaceInvaders Lab

---
+++ includes/game.h
@@ -0,0 +1,123 @@
+
+bool change_direction = false;
+
+class Game : public GameObject
+{
+	std::set<GameObject*> game_objects;	// http://www.cplusplus.com/reference/set/set/
+	
+	AvancezLib* system;
+
+	ObjectPool<Rocket> rockets_pool;	// used to instantiate rockets
+
+	Player * player;
+
+	Sprite * life_sprite;
+	bool game_over;
+
+	unsigned int score = 0;
+
+public:
+
+	virtual void Create(AvancezLib* system)
+	{
+		SDL_Log("Game::Create");
+
+		this->system = system;
+
+		player = new Player();
+		PlayerBehaviourComponent * player_behaviour = new PlayerBehaviourComponent();
+		player_behaviour->Create(system, player, &game_objects, &rockets_pool);
+		RenderComponent * player_render = new RenderComponent();
+		player_render->Create(system, player, &game_objects, "data/player.bmp");
+
+		player->Create();
+		player->AddComponent(player_behaviour);
+		player->AddComponent(player_render);
+		player->AddReceiver(this);
+		game_objects.insert(player);
+
+		rockets_pool.Create(30);
+		for (auto rocket = rockets_pool.pool.begin(); rocket != rockets_pool.pool.end(); rocket++)
+		{
+			RocketBehaviourComponent * behaviour = new RocketBehaviourComponent();
+			behaviour->Create(system, *rocket, &game_objects);
+			RenderComponent * render = new RenderComponent();
+			render->Create(system, *rocket, &game_objects, "data/rocket.bmp");
+			(*rocket)->Create();
+			(*rocket)->AddComponent(behaviour);
+			(*rocket)->AddComponent(render);
+		}
+
+
+		life_sprite = system->createSprite("data/player.bmp");
+		score = 0;
+	}
+
+	virtual void Init()
+	{
+		player->Init();
+
+		enabled = true;
+		game_over = false;
+	}
+
+	virtual void Update(float dt)
+	{
+		if (IsGameOver())
+			dt = 0.f;
+
+		for (auto go = game_objects.begin(); go != game_objects.end(); go++)
+			(*go)->Update(dt);
+	}
+
+	virtual void Draw()
+	{
+		char msg[1024];
+		sprintf(msg, "%07d", Score());
+		system->drawText(300, 32, msg);
+		sprintf(msg, "bonus: %.1fX", game_speed);
+		system->drawText(510, 32, msg);
+
+		for (int i = 0; i < player->lives; i++)
+			life_sprite->draw(i*36+20, 16);
+
+		if (IsGameOver())
+		{
+			sprintf(msg, "*** G A M E  O V E R ***");
+			system->drawText(250, 8, msg);
+		}
+	}
+
+	virtual void Receive(Message m)
+	{
+		if (m == GAME_OVER)
+			game_over = true;
+
+		if (m == ALIEN_HIT)
+			score += POINTS_PER_ALIEN * game_speed;
+	}
+
+
+	bool IsGameOver()
+	{
+		return game_over;
+	}
+
+	unsigned int Score()
+	{
+		return score;
+	}
+
+	virtual void Destroy()
+	{
+		SDL_Log("Game::Destroy");
+
+		for (auto go = game_objects.begin(); go != game_objects.end(); go++)
+			(*go)->Destroy();
+
+		life_sprite->destroy();
+	
+		rockets_pool.Destroy();
+		delete player;
+	}
+};
