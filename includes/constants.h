1e34c1bdf114b56be2cda1060e336a147dc13c21
Henry Yang <mail@xiaoming.se>
2018-08-02 17:11:36 +0800

    Add constants.h for defining constants

---
+++ includes/constants.h
@@ -0,0 +1,21 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const unsigned int	MAX_NUM_GAME_OBJECTS = 10000;
+const unsigned int	NUM_LIVES = 2;
+
+const unsigned int	MAX_NUM_ROCKETS = 32;
+const unsigned int	MAX_NUM_BOMBS = 32;
+const unsigned int	POINTS_PER_ALIEN = 100;
+const float			FIRE_TIME_INTERVAL = .5f;
+const float			BOMB_TIME_INTERVAL = 1.25f;
+const float			PLAYER_SPEED = 160.0f;
+const float			ROCKET_SPEED = 160.0f;
+const float			ALIEN_SPEED = 40.0f;
+const float			BOMB_SPEED = 120.0f;
+
+
+float game_speed = 1.f;		// speed of the game; it is increased each time all the aliens are hit
+							// it is also the score multiplier
+
+#endif
