780366a413b7ca029ea0d32ce41e831d18e505f0
Henry Yang <mail@xiaoming.se>
2018-08-30 18:44:27 +0200

    Add Basic AI behaviourClass

--- includes/behaviour_component.h
+++ includes/behaviour_component.h
@@ -3,6 +3,7 @@
 
 #include "component.h"
 #include "game_map.h"
+#include <queue>
 
 class BehaviourComponent : public Component
 {
@@ -17,5 +18,67 @@ public:
 	}
 };
 
+class AIBehaviourComponent : public BehaviourComponent
+{
+public:
+	struct DirStruct
+	{
+		Direction dir;
+		int ticks;
+		
+		DirStruct(
+			Direction dir,
+			int ticks
+		)
+		{
+			this->dir = dir;
+			this->ticks = ticks;
+		}
 
+		bool operator<(const struct DirStruct& rhs) const
+		{
+			return ticks < rhs.ticks;
+		}
+	};
+	ObjectPool<GameObject> * targets;
+	virtual void Create(AvancezLib* system, GameObject * go, std::set<GameObject*> * game_objects, GameMap * map, ObjectPool<GameObject> * targets)
+	{
+		BehaviourComponent::Create(system, go, game_objects, map);
+
+		this->targets = targets;
+	}
+
+	virtual void Update(float dt)
+	{
+		return;
+	}
+
+	virtual double Heuristics(double x, double y)
+	{
+		return 0.0;
+	}
+
+
+	// Path finding algorithm function
+	virtual Direction FindDirection(
+		double change,
+		GameObject * target
+	)
+	{
+		std::priority_queue<DirStruct> dirs;
+		GameObject goCopy;
+		if (!map->isColliding(Direction::LEFT, change, go))
+		{
+			goCopy.horizontalPosition = go->horizontalPosition - change;
+			goCopy.verticalPosition = go->verticalPosition;
+			dirs.push(DirStruct(Direction::LEFT, CalcDistance(goCopy, *target, Direction::LEFT, change)));
+		}
+			
+	}
+
+	int CalcDistance(GameObject go, GameObject target, Direction dir, double change)
+	{
+		return 0;
+	}
+};
 #endif
