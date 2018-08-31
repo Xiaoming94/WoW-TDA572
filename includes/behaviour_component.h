5e1345ee16025cf30d7f0670f04cac12badac716
Henry Yang <mail@xiaoming.se>
2018-08-31 18:17:49 +0200

    Implement first version of A*

--- includes/behaviour_component.h
+++ includes/behaviour_component.h
@@ -4,6 +4,8 @@
 #include "component.h"
 #include "game_map.h"
 #include <queue>
+#include <cmath>
+#include <vector>
 
 class BehaviourComponent : public Component
 {
@@ -40,6 +42,25 @@ public:
 			return ticks < rhs.ticks;
 		}
 	};
+
+	struct PathNode {
+		GameObject go;
+		Direction lastDir;
+		int ticks;
+		double value; //Ticks + Heuristics
+		PathNode(GameObject go, Direction lastDir,int ticks, double value)
+		{
+			this->go = go;
+			this->value = value;
+			this->ticks = ticks;
+			this->lastDir = lastDir;
+		}
+
+		bool operator<(const struct PathNode& rhs) const
+		{
+			return value < rhs.value;
+		}
+	};
 	ObjectPool<GameObject> * targets;
 	virtual void Create(AvancezLib* system, GameObject * go, std::set<GameObject*> * game_objects, GameMap * map, ObjectPool<GameObject> * targets)
 	{
@@ -50,14 +71,33 @@ public:
 
 	virtual void Update(float dt)
 	{
-		return;
+		
 	}
 
-	virtual double Heuristics(double x, double y)
+	virtual double Heuristics(GameObject go, double x, double y)
 	{
 		return 0.0;
 	}
 
+	void Move(Direction dir,double change)
+	{
+		switch (dir)
+		{
+		case Direction::LEFT :
+			this->go->horizontalPosition -= change;
+			break;
+		case Direction::RIGHT :
+			this->go->horizontalPosition += change;
+			break;
+		case Direction::UP :
+			this->go->verticalPosition -= change;
+			break;
+		case Direction::DOWN :
+			this->go->verticalPosition += change;
+			break;
+		}
+	}
+	// Below this comments are utility functions useful for the AIs
 
 	// Path finding algorithm function
 	virtual Direction FindDirection(
@@ -67,18 +107,130 @@ public:
 	{
 		std::priority_queue<DirStruct> dirs;
 		GameObject goCopy;
-		if (!map->isColliding(Direction::LEFT, change, go))
+		if (!(map->isColliding(Direction::LEFT, change, go)))
 		{
 			goCopy.horizontalPosition = go->horizontalPosition - change;
 			goCopy.verticalPosition = go->verticalPosition;
 			dirs.push(DirStruct(Direction::LEFT, CalcDistance(goCopy, *target, Direction::LEFT, change)));
 		}
+		if (!(map->isColliding(Direction::RIGHT, change, go)))
+		{
+			goCopy.horizontalPosition = go->horizontalPosition - change;
+			goCopy.verticalPosition = go->verticalPosition;
+			dirs.push(DirStruct(Direction::RIGHT, CalcDistance(goCopy, *target, Direction::RIGHT, change)));
+		}
+		if (!(map->isColliding(Direction::UP, change, go)))
+		{
+			goCopy.horizontalPosition = go->horizontalPosition - change;
+			goCopy.verticalPosition = go->verticalPosition;
+			dirs.push(DirStruct(Direction::UP, CalcDistance(goCopy, *target, Direction::UP, change)));
+		}
+		if (!(map->isColliding(Direction::DOWN, change, go)))
+		{
+			goCopy.horizontalPosition = go->horizontalPosition - change;
+			goCopy.verticalPosition = go->verticalPosition;
+			dirs.push(DirStruct(Direction::DOWN, CalcDistance(goCopy, *target, Direction::DOWN, change)));
+		}
+
+		return dirs.top().dir;
 			
 	}
 
 	int CalcDistance(GameObject go, GameObject target, Direction dir, double change)
 	{
-		return 0;
+		std::priority_queue<PathNode> queue;
+		return CalcDistanceAstar(go, target, dir, change, queue, 1);
+	}
+	
+	int CalcDistanceAstar(GameObject go, GameObject target, Direction dir, double change, std::priority_queue<PathNode> &queue, int ticks)
+	{
+		if (AtDestination(go, target))
+			return ticks;
+
+		queue.pop();
+		std::vector<Direction> availableDirs = getAvailableDirections(dir, go, change);
+
+		for (Direction d : availableDirs)
+		{
+			GameObject newGo;
+			newGo.horizontalPosition = go.horizontalPosition;
+			newGo.verticalPosition = go.verticalPosition;
+			switch (d)
+			{
+			case Direction::LEFT :
+				newGo.horizontalPosition -= change;
+				break;
+			case Direction::RIGHT :
+				newGo.horizontalPosition += change;
+				break;
+			case Direction::UP :
+				newGo.verticalPosition -= change;
+				break;
+			case Direction::DOWN :
+				newGo.verticalPosition += change;
+				break;
+			default :
+				break;
+			}
+			PathNode newNode(newGo, d, ticks + 1, ticks + 1 + Heuristics(newGo, target.horizontalPosition, target.verticalPosition));
+			queue.push(newNode);
+		}
+		PathNode nextNode = queue.top();
+		return CalcDistanceAstar(nextNode.go, target, nextNode.lastDir, change, queue, nextNode.ticks);
+	}
+
+	bool AtDestination(GameObject go, GameObject target)
+	{
+		return std::abs(target.horizontalPosition - go.horizontalPosition) < SPRITE_SIDE
+			|| std::abs(target.verticalPosition - go.horizontalPosition) < SPRITE_SIDE;
+	}
+
+	std::vector<Direction> getAvailableDirections(Direction lastDir, GameObject go, double change)
+	{
+		std::vector<Direction> dirs;
+		switch(lastDir)
+		{
+		case Direction::LEFT :
+			if(isAvailableDirection(go, Direction::LEFT, change))
+				dirs.push_back(Direction::LEFT);
+			if(isAvailableDirection(go, Direction::UP, change))
+				dirs.push_back(Direction::UP);
+			if(isAvailableDirection(go, Direction::DOWN, change))
+				dirs.push_back(Direction::DOWN);
+			return dirs;
+		case Direction::RIGHT :
+			if (isAvailableDirection(go, Direction::RIGHT, change))
+				dirs.push_back(Direction::RIGHT);
+			if (isAvailableDirection(go, Direction::UP, change))
+				dirs.push_back(Direction::UP);
+			if (isAvailableDirection(go, Direction::DOWN, change))
+				dirs.push_back(Direction::DOWN);
+			return dirs;
+		case Direction::UP :
+			if (isAvailableDirection(go, Direction::UP, change))
+				dirs.push_back(Direction::UP);
+			if (isAvailableDirection(go, Direction::LEFT, change))
+				dirs.push_back(Direction::LEFT);
+			if (isAvailableDirection(go, Direction::RIGHT, change))
+				dirs.push_back(Direction::RIGHT);
+			return dirs;
+		case Direction::DOWN :
+			if (isAvailableDirection(go, Direction::DOWN, change))
+				dirs.push_back(Direction::UP);
+			if (isAvailableDirection(go, Direction::LEFT, change))
+				dirs.push_back(Direction::LEFT);
+			if (isAvailableDirection(go, Direction::RIGHT, change))
+				dirs.push_back(Direction::RIGHT);
+			return dirs;
+
+		default :
+			return dirs;
+		}
+	}
+
+	bool isAvailableDirection(GameObject go, Direction dir, double change)
+	{
+		return !(map->isColliding(dir, change, &go));
 	}
 };
 #endif
